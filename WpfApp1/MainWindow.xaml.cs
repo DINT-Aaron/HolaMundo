81afdb546b21915a1ebef762351f8c5c11a2c95c
diazAaron <dbaaron@live.com>
2022-09-23 12:43:22 +0200

    Agregar archivos de proyecto.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            nombreTextBlock.Text = nombreTextBox.Text==""? "Hola mundo!": "Hola " + nombreTextBox.Text + "!";
+        }
+    }
+}
